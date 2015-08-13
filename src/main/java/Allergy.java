387ff71d3cebd29dff6c29b0a5c5702cb0592572
jocelyn alsdorf & yvonne peng <student@epicodus.com>
2015-08-13 16:07:13 -0700

    Added the outCome to webpage

--- src/main/java/Allergy.java
+++ src/main/java/Allergy.java
@@ -1,38 +1,31 @@
 
 import java.util.*;
-// import java.io.Console;
-//
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
+import java.io.Console;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class Allergy {
   public static void main(String[] args) {
-    // staticFileLocation("/public");
-    // String layout = "templates/layout.vtl";
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/home.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/whowon", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/whowon.vtl");
-    //
-    //   String playerOne = request.queryParams("playerOne");
-    //   String playerTwo = request.queryParams("playerTwo");
-    //
-    //   String whoWins = whoWins(playerOne, playerTwo);
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
-   allergyName(555);
+    get("/outcome", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/outcome.vtl");
 
+      String score = request.queryParams("score");
 
-    //   model.put("playerOne", playerOne);
-    //   model.put("playerTwo", playerTwo);
-    //   model.put("whoWins", whoWins(playerOne, playerTwo));
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+      Integer intScore = Integer.parseInt(score);
+      model.put("allergyName", allergyName(intScore));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 
